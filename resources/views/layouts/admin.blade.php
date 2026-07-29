a642f76f4e0db87f69505723294dcdeab5697e22
David Ngungila <davidngungila@gmail.com>
2026-07-29 21:20:34 +0300

    Add Loan Products menu to Loans dropdown in sidebar
    - Add Loan Products link to Loans dropdown menu
    - Link points to admin.products.index route
    - Shows active state when on products pages

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -133,6 +133,12 @@
             <i class="fa-solid fa-money-bill-transfer w-4 text-center flex-shrink-0"></i>
             <span x-show="!sidebarCollapsed" class="font-medium whitespace-nowrap">Loan Repayments</span>
           </a>
+          <a href="{{ route('admin.products.index') }}"
+             class="sidebar-item w-full flex items-center gap-3 px-3 py-2 rounded-lg text-sm text-primary-300 hover:text-white transition-all duration-150
+                    {{ request()->routeIs('admin.products.*') ? 'active' : '' }}">
+            <i class="fa-solid fa-box w-4 text-center flex-shrink-0"></i>
+            <span x-show="!sidebarCollapsed" class="font-medium whitespace-nowrap">Loan Products</span>
+          </a>
         </div>
       </div>
 
