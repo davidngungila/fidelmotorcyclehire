911a615831d991042b5325d631c23da40637bf66
David Ngungila <davidngungila@gmail.com>
2026-08-02 12:25:10 +0300

    Increase z-index to ensure appreciation certificate modal displays in front
    - Change z-index from z-50 to z-[9999] for appreciation certificate modal
    - Ensures modal displays in front of all other page elements

--- resources/views/admin/loans/show.blade.php
+++ resources/views/admin/loans/show.blade.php
@@ -59,7 +59,7 @@
       </div>
 
       <!-- Appreciation Certificate Preview Modal -->
-      <div id="appreciationCertificateModal" class="fixed inset-0 bg-black bg-opacity-50 hidden items-center justify-center z-50">
+      <div id="appreciationCertificateModal" class="fixed inset-0 bg-black bg-opacity-50 hidden items-center justify-center z-[9999]">
         <div class="bg-white dark:bg-dark-card rounded-xl shadow-2xl max-w-4xl w-full mx-4 max-h-[90vh] overflow-auto">
           <div class="p-6">
             <div class="flex items-center justify-between mb-4">
