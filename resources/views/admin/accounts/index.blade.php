45ac48bfc38cf5383def89868dd56a48326bfb40
David Ngungila <davidngungila@gmail.com>
2026-08-03 13:45:30 +0300

    Fix Blade syntax error in admin accounts index
    - Replace @empty directive with @if(->isEmpty())
    - Fixes ParseError with unexpected endforeach token
    - Properly closes conditional directives

--- resources/views/admin/accounts/index.blade.php
+++ resources/views/admin/accounts/index.blade.php
@@ -83,7 +83,8 @@
                 </div>
               </td>
             </tr>
-          @empty
+          @endforeach
+          @if($accounts->isEmpty())
             <tr>
               <td colspan="7" class="text-center py-12 text-gray-500 dark:text-gray-400">
                 <i class="fa-solid fa-book text-3xl mb-3 block opacity-30"></i>
@@ -91,7 +92,7 @@
                 <p class="text-xs">Create your first account to get started</p>
               </td>
             </tr>
-          @endforeach
+          @endif
         </tbody>
       </table>
     </div>
